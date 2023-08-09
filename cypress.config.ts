46c941a574ebe49f8bb8a4661c2103c56338f07d
Ahsan Mahmood <aoneahsan@gmail.com>
2023-08-09 21:28:14 +0500

    cypress/v1 - project ready for testing, this commit can serve as starting point for next project cypress testing setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    projectId: "z8vgeu",
     baseUrl: "https://prettylinks.zaions.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
